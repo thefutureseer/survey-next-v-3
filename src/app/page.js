19cf8d392501dd5d77db5911a41d3bceed03e453
thefutureseer <danielharrislimos@outlook.com>
2024-05-03 23:53:45 -0700

    serve the survey component

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import surveyJson from "../data/surveyContainer.json";
+import SurveyContainer from "./surveyContainer";
 
 export default function Home() {
   return (
    <body>
-    <main className={styles.main}>
-      Hello Next
-     { surveyJson? " World!" : "no survey at this time!"}
-    </main>
+      <SurveyContainer />
    </body> 
   );
 }
