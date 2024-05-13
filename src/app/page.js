23d52024394bb6725475f560935413332f9b6a30
thefutureseer <danielharrislimos@outlook.com>
2024-05-13 02:42:04 -0700

    add button to see results

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-import styles from "./page.module.css";
-import SurveyContainer from "./surveyContainer";
+// import styles from "./page.module.css";
+import SurveyContainer from "./components/surveyContainer";
+import Link from 'next/Link';
 
 export default function Home() {
   return (
    <body>
-      <SurveyContainer />
+    <Link href="/api/results">
+      <button>See all results</button>
+    </Link>
+    <SurveyContainer />
    </body> 
   );
 }
