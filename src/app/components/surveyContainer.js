94221d928306557895427be0b69e772d5dc6a03d
thefutureseer <danielharrislimos@outlook.com>
2024-05-12 21:55:12 -0700

    clean up  code

--- src/app/components/surveyContainer.js
+++ src/app/components/surveyContainer.js
@@ -12,10 +12,6 @@ import { Model } from "survey-core";
 // Import survey JSON data
 import surveyJson from "../../data/surveyBones";
 
-// Load environment variables from .env file
-import dotenv from 'dotenv';
-dotenv.config();
-
 // Define SurveyContainer functional component
 const SurveyContainer = () => {
   // Create a new survey model using the survey JSON data
@@ -25,33 +21,10 @@ const SurveyContainer = () => {
   survey.onComplete.add(function (sender, options) {
     // Display the "Saving..." message (pass a string value to display a custom message)
     options.showSaveInProgress();
-
-    // Create a new XMLHttpRequest object for making the POST request
-    const xhr = new XMLHttpRequest();
-    
-    console.log("MongoDB Connection String:", process.env.mongoSurveyDB);
-    // Open a POST request to the MongoDB server using the connection string from environment variables
-    xhr.open("POST", process.env.mongoSurveyDB);
-
-    // Set the request header to indicate JSON content
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-
-    // Define the onload and onerror event handlers for the request
-    xhr.onload = xhr.onerror = function () {
-      // Check the status code of the response
-      if (xhr.status == 200) {
-        // Display the "Success" message (pass a string value to display a custom message)
-        options.showSaveSuccess();
+    options.showSaveSuccess();
         // Alternatively, you can clear all messages:
         // options.clearSaveMessages();
-      } else {
-        // Display the "Error" message (pass a string value to display a custom message)
-        options.showSaveError();
-      }
-    };
 
-    // Send the survey data as a JSON string in the request body
-    xhr.send(JSON.stringify(sender.data));
     
     // Log the sent survey results to the console
     console.log(" survey results", sender.data);
@@ -62,4 +35,4 @@ const SurveyContainer = () => {
 }
 
 // Export the SurveyContainer component
-export default SurveyContainer;
+export default SurveyContainer
