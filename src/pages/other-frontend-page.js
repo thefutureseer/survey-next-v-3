0aaff699b274154e165a9a4ba4689d022a1bedc3
thefutureseer <danielharrislimos@outlook.com>
2024-05-19 07:01:51 -0700

    looking at the different servers and api

---
+++ src/pages/other-frontend-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FrontendPage() {
+  return (
+    <div>
+      <h1>Frontend Page</h1>
+      <p>This is a frontend page using the Pages Router.</p>
+      <p>The problem is if this file name is the same as the one in src/app then that one will take precedence.</p>
+    </div>
+  );
+};
