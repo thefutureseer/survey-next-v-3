868bdaf3e711763ac86d3880986705f03175bdc1
thefutureseer <danielharrislimos@outlook.com>
2024-05-29 05:30:16 -0700

    add fun survey

--- src/data/surveyBones.js
+++ src/data/surveyBones.js
@@ -1,67 +1,89 @@
 const surveyJson = {
- "title": "Survey for deeper results",
- "logoPosition": "right",
- "pages": [
-  {
-   "name": "page1",
-   "elements": [
+  "title": "Survey for deeper results",
+  "logoPosition": "right",
+  "pages": [
     {
-     "type": "checkbox",
-     "name": "Q1 Check whichever",
-     "choices": [
-      "Item 1",
-      "Item 2",
-      "Item 3"
-     ],
-     "showOtherItem": true,
-     "showNoneItem": true,
-     "showSelectAllItem": true
-    },
-    {
-     "type": "radiogroup",
-     "name": "Q2 Check one",
-     "choices": [
-      "Item 1",
-      "Item 2",
-      "Item 3"
-     ],
-     "showOtherItem": true,
-     "showNoneItem": true
-    },
-    {
-     "type": "ranking",
-     "name": "Q3 Rank",
-     "choices": [
-      "Item 1 goes here. ",
-      "Item 2 goes here. ",
-      "Item 3 goes here. "
-     ]
-    },
-    {
-     "type": "rating",
-     "name": "Q4 Rating"
-    },
-    {
-     "type": "tagbox",
-     "name": "Q5 Add/delete, order your answer",
-     "choices": [
-      "Item 1. ",
-      "Item 2. ",
-      "Item 3. "
-     ]
+      "name": "mainPage",
+      "elements": [
+        {
+          "type": "text",
+          "name": "respondentName",
+          "title": "What is your name or code name for this survey?"
+        },
+        {
+          "type": "comment",
+          "name": "teamPerformance",
+          "title": "How well did our team do in yesterday’s game?"
+        },
+        {
+          "type": "text",
+          "name": "supportTeam",
+          "title": "Which sports team do you support?"
+        },
+        {
+          "type": "boolean",
+          "name": "vampireHighSchool",
+          "title": "If you were a vampire, would you attend high school for the rest of your life?",
+          "label": "Yes"
+        },
+        {
+          "type": "comment",
+          "name": "forgetMemory",
+          "title": "Which of your childhood memories would you like to forget?"
+        },
+        {
+          "type": "text",
+          "name": "favoriteToy",
+          "title": "What was your favorite childhood toy or game?"
+        },
+        {
+          "type": "text",
+          "name": "comfortFood",
+          "title": "What's your favorite comfort food?"
+        },
+        {
+          "type": "radiogroup",
+          "name": "snackPreference",
+          "title": "Do you prefer sweet or savory snacks?",
+          "choices": ["Sweet", "Savory"]
+        },
+        {
+          "type": "text",
+          "name": "dreamDestination",
+          "title": "What's your dream travel destination?"
+        },
+        {
+          "type": "comment",
+          "name": "memorableTravel",
+          "title": "Share a memorable travel experience you've had."
+        },
+        {
+          "type": "radiogroup",
+          "name": "petPreference",
+          "title": "Are you a cat person or a dog person?",
+          "choices": ["Cat", "Dog"]
+        },
+        {
+          "type": "text",
+          "name": "reincarnationAnimal",
+          "title": "If reincarnation were real, what animal would you like to come back as?"
+        },
+        {
+          "type": "radiogroup",
+          "name": "datingPreference",
+          "title": "Have you ever had that persistent person who is always trying to catch a date with you while you are single and ready to mingle?",
+          "choices": ["All the time", "Once", "That one time doesn't count"],
+          "hasOther": true,
+          "otherText": "Other (please specify)"
+        },
+        {
+          "type": "boolean",
+          "name": "Love",
+          "title": "Do you love me or do you love me?",
+          "label": "Yes"
+        },
+      ]
     }
-   ]
-  },
-  {
-   "name": "page2",
-   "elements": [
-    {
-     "type": "boolean",
-     "name": "Q6 True or False"
-    }
-   ]
-  }
- ]
-}
-
+  ]
+};
 export default surveyJson;
