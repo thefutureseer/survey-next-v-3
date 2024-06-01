a73e6bcb94a9becaaf3c9fb616d91b2596d946bf
thefutureseer <danielharrislimos@outlook.com>
2024-05-31 22:23:30 -0700

    fix first click

--- src/app/components/ResultsButtonClient.js
+++ src/app/components/ResultsButtonClient.js
@@ -1,27 +1,34 @@
 'use client';
+
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+
 const ResultsButtonClient = () => {
   const router = useRouter();
-  const [isResultsPage, setIsResultsPage] = useState(false);
+  const [isResultsPage, setIsResultsPage] = useState(null);
+
   useEffect(() => {
-    if (window.location.pathname === '/results') {
-      setIsResultsPage(true);
-    } else {
-      setIsResultsPage(false);
-    }
+    setIsResultsPage(window.location.pathname !== '/');
   }, []);
+
   const handleClick = () => {
-    if (isResultsPage) {
-      router.push('/');
-    } else {
+    if (isResultsPage === null) return; // Early return if state is not set yet
+    if (!isResultsPage) {
       router.push('/results');
+    } else {
+      router.push('/');
     }
   };
+
+  if (isResultsPage === null) {
+    return <button disabled>Loading...</button>;
+  }
+
   return (
     <button onClick={handleClick}>
       {isResultsPage ? 'Survey' : 'Results'}
     </button>
   );
 };
+
 export default ResultsButtonClient;
