a07b290dc2feecc26c05c4b039c8269290f82746
thefutureseer <danielharrislimos@outlook.com>
2024-06-02 04:57:15 -0700

    move css to module

--- src/app/components/ResultsList.js
+++ src/app/components/ResultsList.js
@@ -1,15 +1,17 @@
 'use client';
 import React from 'react';
+import '../../styles/resultsList.css';
 
 const ResultsList = ({ results }) => {
+  // alert(results);
   if (!results || results.length === 0) return <p>No results found.</p>;
   const colors = ['red', 'yellow', '#4FFFB0', 'blue',  'purple', 'orange', 'magenta'];
   return (
     <div>
-      <h1 style={{textAlign: "center"}}>Total Survey Results</h1>
-      <ul style={{background: "black"}}>
+      <h2>Total Survey Results</h2>
+      <ul>
         {results.map((result, index) => (
-          <li style={{textAlign:"center", width: '50%', listStyleType: 'none' }} key={result._id}> <h3 style={{color: colors[index % colors.length], borderTop: '4px solid gold', background:"#0087ff" }}>{index}</h3>
+          <li key={result._id}> <h3 style={{color: colors[index % colors.length], borderTop: '4px solid gold', background:"#0087ff" }}>{index}</h3>
             <pre>{
               Object.entries(result).map(([key, value], i)=>(
                 <div key={i} style={{display:'flex', color:colors[i % colors.length]}}>
