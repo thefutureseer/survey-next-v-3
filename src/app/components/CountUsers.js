d24912e30cc2befefc341638198add5122ef9f66
thefutureseer <danielharrislimos@outlook.com>
2024-06-03 05:48:55 -0700

    implement head count algo

---
+++ src/app/components/CountUsers.js
@@ -0,0 +1,15 @@
+const countIdKeys = (inputJson) => {
+  let idCount = 0;
+
+  let arrFromJson = JSON.parse(inputJson);
+
+  for (let obj of arrFromJson) {
+    if (obj.hasOwnProperty('_id')) {
+      idCount++;
+    }
+  }
+
+  return idCount;
+};
+
+module.exports = countIdKeys;
--- src/app/components/ResultsList.js
+++ src/app/components/ResultsList.js
@@ -1,14 +1,18 @@
 'use client';
 import React from 'react';
 import '../../styles/resultsList.css';
+import countIdKeys from './CountUsers';
 
 const ResultsList = ({ results }) => {
-  // alert(results);
+  const result = results;
+  // console.log(JSON.stringify(result));
   if (!results || results.length === 0) return <p>No results found.</p>;
+  const headCount = countIdKeys(JSON.stringify(results));
   const colors = ['red', 'yellow', '#4FFFB0', 'blue',  'purple', 'orange', 'magenta'];
   return (
     <div>
       <h2>Total Survey Results</h2>
+      <p>Surveys taken: {headCount}</p>
       <ul>
         {results.map((result, index) => (
           <li key={result._id}> <h3 style={{color: colors[index % colors.length], borderTop: '4px solid gold', background:"#0087ff" }}>{index}</h3>
