ea694d3f71c7af2b8e5410526454964d85b83aa6
thefutureseer <danielharrislimos@outlook.com>
2024-06-25 20:59:31 -0700

    Revert "Delete src/data/voteScript.js"
    This reverts commit 313593e9ef1fcad084d730170b0ffd3ce2e7d9cf.

---
+++ src/data/voteScript.js
@@ -0,0 +1,17 @@
+const voteJson = {
+  "title": "Vote for best results",
+  "logoPosition": "right",
+  "pages": [
+    {
+      "name": "mainPage",
+      "elements": [
+        {
+          "type": "text",
+          "name": "respondentName",
+          "title": "What is your name or code name for this vote?"
+        }
+      ]
+    }
+  ]
+};
+export default voteJson;
