b8203fa90063ef94c19be8882c6a96ce1c97c7b8
thefutureseer <danielharrislimos@outlook.com>
2025-04-17 23:55:35 -0700

    feat: install app

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import SurveyContainer from "./components/surveyContainer";
 import ResultsButtonClient from '@components/ResultsButtonClient';
+// import '../styles/survey.css';
 
 export default function Home() {
   return (
    <div>
+    <button className="install-btn" style={{ backgroundColor: '#b4eeb6' }}>
+      📲 Install App
+    </button>
     <ResultsButtonClient/>
     <SurveyContainer />
    </div> 
